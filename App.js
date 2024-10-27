10209d7e08e7b3d1e75b331c775bba83ca541f3b
elisamarjut <117983133+elisamarjut@users.noreply.github.com>
2024-10-27 13:54:23 +0200

    First commit
    Initializes the project and creates tab navigation with three screens: home, search and favorites

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/HomeScreen';
+import SearchScreen from './screens/SearchScreen';
+import FavoritesScreen from './screens/FavoritesScreen';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Search') {
+              iconName = 'search';
+            } else if (route.name === 'Favorites') {
+              iconName = 'favorite';
+            }
+
+            return <MaterialIcons name={iconName} size={size} color={color} />;
+          },
+        })}>
+        <Tab.Screen name='Home' component={HomeScreen} />
+        <Tab.Screen name='Search' component={SearchScreen} />
+        <Tab.Screen name='Favorites' component={FavoritesScreen} />
+      </Tab.Navigator>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
+
   );
 }
 
